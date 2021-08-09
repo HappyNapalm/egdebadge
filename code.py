7f9badae5bb7b4827cfcfc47b8f2723f54e8a727
Church <maevestring@gmail.com>
2021-08-08 17:43:48 -0700

    Acceleratometer reading is false. Read the library

---
+++ code.py
@@ -0,0 +1,24 @@
+from adafruit_pybadger import PyBadger
+
+pybadger = PyBadger()
+
+pybadger.auto_dim_display(delay=30)
+
+first_display = True
+
+
+
+while True:
+
+    #check if we are accelerating.
+    name = str(pybadger._check_for_movement())
+
+    if pybadger.button.a:
+        pybadger.show_business_card(image_name="supercon.bmp", name_string="Changeme in code.py", name_scale=1,
+                                    email_string_one="myemail@hackaday.io",
+                                    email_string_two="https://hackaday.io/")
+    elif pybadger.button.b:
+        pybadger.show_qr_code(data="https://hackaday.io/superconference/")
+    elif pybadger.button.start or first_display:
+        pybadger.show_badge(name_string=name, hello_scale=2, my_name_is_scale=2, name_scale=2)
+        first_display = False
